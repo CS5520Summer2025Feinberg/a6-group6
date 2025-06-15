64b17333cb5d54c7c89e392be214ce43f45cffda
Ved Deore <veddeore2312@gmail.com>
2025-06-14 20:00:31 -0400

    Reformat

--- app/src/main/java/edu/northeastern/a6_assignments/activities/MainActivity.java
+++ app/src/main/java/edu/northeastern/a6_assignments/activities/MainActivity.java
@@ -14,20 +14,20 @@ import edu.northeastern.a6_assignments.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    EdgeToEdge.enable(this);
+    setContentView(R.layout.activity_main);
+    ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+      Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+      v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+      return insets;
+    });
+  }
 
-    public void onClickService(View view) {
-        Intent intent = new Intent(MainActivity.this, FoodRecipeRequestActivity.class);
-        startActivity(intent);
-    }
+  public void onClickService(View view) {
+    Intent intent = new Intent(MainActivity.this, FoodRecipeRequestActivity.class);
+    startActivity(intent);
+  }
 }
