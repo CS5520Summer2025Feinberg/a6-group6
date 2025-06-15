c1c31857c9bf120139027aa32a5aef69ad6a51b8
Vicky Daniel <vicky16898@gmail.com>
2025-06-15 14:56:36 -0400

    Add List Adapter To Display Recipes

---
+++ app/src/main/java/edu/northeastern/a6_assignments/pojo/Recipe.java
@@ -0,0 +1,52 @@
+package edu.northeastern.a6_assignments.pojo;
+
+import java.io.Serializable;
+
+public class Recipe implements Serializable {
+
+    private int id;
+    private String title;
+    private String image;
+    private String imageType;
+
+    public Recipe() {}
+
+    public Recipe(int id, String title, String image, String imageType) {
+        this.id = id;
+        this.title = title;
+        this.image = image;
+        this.imageType = imageType;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getImageType() {
+        return imageType;
+    }
+
+    public void setImageType(String imageType) {
+        this.imageType = imageType;
+    }
+}
